58965f83160868fdb8cb7ee340ec200ccb02d4df
Ivan Miranda <ivan.i.n@hotmail.com>
2015-11-08 12:56:32 -0200

    Loaders functions

---
+++ src/loaders/loaders.js
@@ -0,0 +1,40 @@
+/**
+ @author: betobyte / threescript.com
+ @author: ivan / threescript.com
+ */
+
+/**
+ * Load a THREEjs JSON file
+ * @param {String} url - url to json file
+ * @param {function} onLoad - onLoad callback
+ * @param {function} onProgress - onProgress callback
+ * @param {function} onError - onError callback
+ */
+var load_json = function( url, onLoad, onProgress, onError ) {
+	var loader = new THREE.JSONLoader();
+	loader.load(url, onLoad, onProgress, onError);
+};
+
+/**
+ * Load a texture file
+ * @param {String} url - url to texture file
+ * @param {function} onLoad - onLoad callback
+ * @param {function} onProgress - onProgress callback
+ * @param {function} onError - onError callback
+ */
+var load_texture = function( url, onLoad, onProgress, onError ) {
+	var loader = new THREE.TextureLoader();
+	loader.load(url, onLoad, onProgress, onError);
+};
+
+/**
+ * Load a material file
+ * @param {String} url - url to material file
+ * @param {function} onLoad - onLoad callback
+ * @param {function} onProgress - onProgress callback
+ * @param {function} onError - onError callback
+ */
+var load_material = function( url, onLoad, onProgress, onError ) {
+	var loader = new THREE.MaterialLoader();
+	loader.load(url, onLoad, onProgress, onError);
+};
